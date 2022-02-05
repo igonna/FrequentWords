cf43dc325fd3ad6f7dfcf7eafcf7674f472dbbd0
igonna <northearts@gmail.com>
2022-02-05 03:50:16 -0800

    dirty, but fast

--- FrequentWords/FrequentWords.cpp
+++ FrequentWords/FrequentWords.cpp
@@ -1,20 +1,171 @@
-// FrequentWords.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+#include <streambuf>
+#include <ctime>
+#include <chrono>
 
-int main()
+namespace user
 {
-    std::cout << "Hello World!\n";
+	class File
+	{
+	private:
+		std::string Data;
+	public:
+		File(std::string fileName)
+		{
+			Data = getStringFromFile(fileName);
+		}
+
+		std::string getStringFromFile(std::string fileName)
+		{
+			std::ifstream ifile(fileName);
+			if (!ifile)
+				return std::string();
+
+			std::ostringstream ss;
+			ss << ifile.rdbuf();
+			return ss.str();
+		}
+
+		operator std::string() const
+		{
+			return Data;
+		}
+	};
+
+	class WordsFrequent
+	{
+	private:
+		struct Word
+		{
+			std::string str;
+			size_t n;
+			Word(std::string str, size_t n)
+			{
+				this->str = str;
+				this->n = n;
+			}
+		};
+		std::vector<Word> WordsFrequent;
+		size_t it;
+	public:
+		bool containsWord(std::string word)
+		{
+			for (size_t i = 0; i < WordsFrequent.size(); ++i)
+			{
+				if (WordsFrequent[it = i].str == word)
+					return true;
+			}
+			return false;
+		}
+		void addWord(std::string word)
+		{
+			if (containsWord(word))
+			{
+				++WordsFrequent[it].n;
+
+			}
+			else
+			{
+				WordsFrequent.push_back(Word(word, 1));
+			}
+		}
+		size_t size()
+		{
+			return WordsFrequent.size();
+		}
+		std::vector<std::string> getSortedWords()
+		{
+			std::sort(WordsFrequent.begin(), WordsFrequent.end(), [](Word a, Word b)
+				{
+					return a.n > b.n;
+				});
+			std::vector<std::string> wordsSorted;
+			for (auto w : WordsFrequent)
+			{
+				wordsSorted.push_back(w.str);
+			}
+			return wordsSorted;
+		}
+	};
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+bool isAsciiChar(char c)
+{
+	return (90 >= static_cast<int>(c) && static_cast<int>(c) >= 65) || (122 >= static_cast<int>(c) && static_cast<int>(c) >= 97) || c == '\'';
+}
+
+std::string cleanString(std::string str, bool (*ValidChar)(char c))
+{
+	std::string StrClean;
+	for (size_t i = 0; i < str.size(); ++i)
+	{
+		if (str[i] == ' ')
+		{
+			if (i && str[i - 1] != ' ')
+				StrClean += str[i];
+			continue;
+		}
+		if (ValidChar(str[i]))
+			StrClean += str[i];
+	}
+	return StrClean;
+}
+
+std::vector<std::string> getWordsFromString(std::string str)
+{
+	std::vector<std::string> Lines;
+	char SymbolEndOfLine = ' ';
+
+	std::string Line;
+	for (size_t i = 0; i < str.length(); ++i)
+	{
+		if (str[i] != SymbolEndOfLine)
+			Line += str[i];
+		else
+		{
+			Lines.push_back(Line);
+			Line.clear();
+		}
+	}
+	return Lines;
+}
+
+
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+		return 1;
+
+	auto TimeStart = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+
+	std::string DataDirty = user::File(argv[1]);
+	DataDirty = cleanString(DataDirty, isAsciiChar);
+	std::vector<std::string> Words = getWordsFromString(DataDirty);
+	user::WordsFrequent WordsFrequent;
+
+	for (auto word : Words)
+	{
+		WordsFrequent.addWord(word);
+	}
+	std::string StrWords;
+	for (auto word : WordsFrequent.getSortedWords())
+	{
+		StrWords += word + "\n";
+	}
+	std::ofstream("C:/Users/c/Downloads/finalFile.txt") << StrWords;
+	
+	std::cout << "Words: " << Words.size() << " Freq: " << WordsFrequent.size() << std::endl;
+
+	auto TimeExec = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count() - TimeStart;
+	std::cout << "Execution time: " << static_cast<double>(TimeExec) / 1000.0 << " sec." << std::endl;
+    return 0;
+}
